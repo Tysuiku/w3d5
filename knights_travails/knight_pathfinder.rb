bdc1987502066d7098fd8a1e7d54f282e27594b8
luckyleck <luckyleck29@gmail.com>
2023-01-30 11:32:04 -0500

    Test files

--- knights_travails/knight_pathfinder.rb
+++ knights_travails/knight_pathfinder.rb
@@ -1,4 +1,26 @@
+
+
 class KnightPathfinder
+
+  attr_reader :root_node , :children, :parent
+  
   def initialize
+    @root_node = [0,0]
+    @children = []
+    @parent = nil
   end
+
+  def parent=(parent_node)
+    if @parent != nil
+      @parent.children.delete(self)
+    end
+
+    @parent = parent_node
+
+    parent_node.children << self unless pnode == nil
+  end
+
+  
+
+
 end
---
+++ test/test.rb
@@ -0,0 +1,25 @@
+class Node
+
+    attr_accessor :value, :parent, :children
+
+    def initialize(value)
+        @value = value
+        @parent = parent
+        @children = []
+    end
+
+    self.show_node(node)
+
+    puts node
+    node.children
+
+
+end
+
+# a = Node.new("a")
+# b = Node.new("b")
+# c = Node.new("c")
+
+
+
+    
