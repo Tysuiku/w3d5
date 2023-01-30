8e03d2007ef3d32d731283d92a339f526ed4c902
Tysuiku <Timothydong41@gmail.com>
2023-01-30 17:09:29 -0500

    add build_move_tree

--- knights_travails/knight_pathfinder.rb
+++ knights_travails/knight_pathfinder.rb
@@ -1,6 +1,7 @@
 require_relative "PolyTree.rb"
 
 class KnightPathFinder
+  attr_accessor :start_pos, :possible_pos
   def self.valid_moves(pos)
     possible_moves = [[1, 2], [2, 1], [-1, -2], [-2, -1], [-2, 1], [-1, 2], [2, -1], [1, -2]]
     good_moves = []
@@ -20,19 +21,31 @@ class KnightPathFinder
   end
 
   def new_move_positions(pos)
-
     returned_pos = KnightPathFinder.valid_moves(pos)
-    returned_pos.delete_if {|ele| ele.nil? || @possible_pos.include?(ele)}
+    returned_pos.delete_if { |ele| @possible_pos.include?(ele) }
 
     returned_pos.each do |coordinate|
       @possible_pos << coordinate unless @possible_pos.include?(coordinate)
     end
 
     returned_pos
-
   end
 
-    
+  def build_move_tree
+    queue = [@start_pos]
 
+    until queue.empty?
+      current_node = queue.shift
+      current_pos = current_node.value
 
+      nmp = new_move_positions(current_pos)
+
+      nmp.each do |next_pos|
+        next_node = PolyTreeNode.new(next_pos)
+        current_node.add_child(next_node)
+        queue << next_node
+      end
+    end
+    puts "success"
+  end
 end
